7a00ece29911122a088f6a73f8878a2751658b2c
Emanuele <cannone.emanuele@gmail.com>
2021-01-25 14:14:56 +0100

    Init

---
+++ vue/script.js
@@ -0,0 +1,3 @@
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+// Bonus: Aggiungere un’immagine presa anch’essa da un data assieme al valore alt dell'attributo. 
+// Poi aggiungete all'immagine una classe sempre presa dal data.
